b832db62671091f363f9e9718e432b7ff19966ac
Ki Ki  Bush <donyabush@gmail.com>
2018-06-08 20:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function whisper(sting) {
+
+function whisper(string) {
   return string.toLowerCase()
 }
 
-var upperCase = "HELLO!"
-upperCase.toUpperCase() === uppercase // true
+function sayHiToGrandma(string){
+if (string === string.toLowerCase()){
+return ("I can't hear you!")
+}
+else if (string===string.toUpperCase()) {
+return ("YES INDEED!")
+}
+else {
+return ("I love you, too.")
+}
+}
+ 
